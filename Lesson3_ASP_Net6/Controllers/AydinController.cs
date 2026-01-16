f303f7d87406276629332c8d73e052e4729f4c0c
karimzade_k <karimzade_k@STHQ0118-01>
2026-01-16 21:03:51 +0400

    view elave edildi

---
+++ Lesson3_ASP_Net6/Controllers/AydinController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson3_ASP_Net6.Controllers;
+
+public class AydinController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
